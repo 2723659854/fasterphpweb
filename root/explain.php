e90828eff620c4a614bf95e0321b84d58ca311c7
songshu <2723659854@qq.com>
2022-07-10 21:01:41 +0800

    测试图片类文件上传

--- root/explain.php
+++ root/explain.php
@@ -190,7 +190,7 @@ class HttpServer
                     if (strstr($b,'image')){
                         $start=$a+1;
                         for($ii=$start;$ii<=$value_key_stop;$ii++){
-                            $value=$value.$now[$ii];
+                            if ($value.$now[$ii])$value=$value.$now[$ii];
                         }
                     }else{
                         $start=$a+2;
