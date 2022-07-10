21c741583896f176cf1e5f97c4bae16ad459f24c
songshu <2723659854@qq.com>
2022-07-10 19:12:40 +0800

    测试

--- app/timer/OtherTimer.php
+++ app/timer/OtherTimer.php
@@ -9,6 +9,6 @@ class OtherTimer
     //定时器的业务逻辑必须写在handle方法里面，然后需要在config/timer里面配置
     public function handle(){
         //测试写入文件
-        file_put_contents(app_path().'/public/book/'.time().'note.txt','搜索');
+        file_put_contents(app_path().'/public/'.time().'note.txt','搜索');
     }
 }
--- app/timer/Test.php
+++ app/timer/Test.php
@@ -4,7 +4,7 @@ namespace App\Time;
 class Test{
     //定时器的业务逻辑必须写在handle方法里面，然后需要在config/timer里面配置
     public function handle(){
-        file_put_contents(app_path().'/public/book/'.time().'book.txt','搜索');
+        file_put_contents(app_path().'/public/'.time().'book.txt','搜索');
     }
 
 }
