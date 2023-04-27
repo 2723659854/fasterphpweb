ddee2523575b6e4d65cd69ab660bcbb9a71192ee
yanglong <2723659854@qq.com>
2023-04-27 18:09:00 +0800

    删除测试文件

--- app/timer/OtherTimer.php
+++ app/timer/OtherTimer.php
@@ -6,7 +6,7 @@ class OtherTimer
     //定时器的业务逻辑必须写在handle方法里面，然后需要在config/timer里面配置
     public function handle(){
         //测试写入文件
-        file_put_contents(app_path().'/public/'.time().'note.txt','搜索');
+        //file_put_contents(app_path().'/public/'.time().'note.txt','搜索');
         var_dump("我是定时器".date('Y-m-d H:i:s'));
     }
 }
